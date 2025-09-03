1a48ddf01c876922b112ae33cc066e2c747e2c10
OOTSUKAMAMORU <k024g0018@g.neec.ac.jp>
2025-09-03 12:10:42 +0900

    TR1_1

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
-	int timer = 60 * 10;
+	int timer = 60 * 30;
 
 	//プレイヤー
 	int posX = 200;
@@ -24,15 +24,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//0は存在しない１は存在する
 	
 	//敵
-	int enemyX=100;
-	int enemyY = 100;
+	const int enemyMax = 5;
+	int enemyX[enemyMax] = {0};
+	int enemyY[enemyMax] = { 0 };
+	int enemyFlag[enemyMax] = { 0 };
+
 
 	//スコア
 	int  score = 0;
 
 	// 現在時刻の情報で初期化
 	srand((unsigned int)time(NULL));
-	enemyX = rand() % 1100;
+	//enemyX = rand() % 1100;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -45,25 +48,48 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		if (timer >= 0)
 		{
-
-			//衝突判定
-			if (abs(enemyX - bulletX) < 50 && abs(enemyY - bulletY) < 50)
+			//敵の発生
+			if (rand() % 20 == 0)
 			{
-				enemyY = 0;
-				enemyX = rand() % 1100;
+				for (int i = 0; i < enemyMax; i++)
+				{
+					if (enemyFlag[i] == 0)
+					{
+						enemyFlag[i] = 1;
+						enemyY[i] = 0;
+						enemyX[i] = rand() % 1100;
+						break;
+					}
+				}
+			}
+			//衝突判定
+			for (int i = 0; i < enemyMax; i++)
+			{
+				if (enemyFlag[i] == 1)
+				{
+					if (abs(enemyX[i] - bulletX) < 50 && abs(enemyY[i] - bulletY) < 50)
+					{
+						enemyFlag[i] = 0;
+					}
+				}
 			}
 			///
 			/// ↓更新処理ここから
 			///
 
 			//敵が落ちてくるスピード
-			enemyY += 5;
-
-			//敵が画面下に行ったら上に戻る
-			if (enemyY > 720)
+			for (int i = 0; i < enemyMax; i++)
 			{
-				enemyY = 0;
-				enemyX = rand() % 1100;
+				if (enemyFlag[i] == 1)
+				{
+					enemyY[i] += 5;
+
+					//敵が画面下に行ったら上に戻る
+					if (enemyY[i] > 720)
+					{
+						enemyFlag[i] = 0;
+					}
+				}
 			}
 			//プレイヤーの移動
 			if (keys[DIK_D]) 
@@ -76,18 +102,27 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			}
 
 			//当たり判定
-			if (abs(bulletX - enemyX) < 100 && abs(bulletY - enemyY) < 100)
+			for (int i = 0; i < enemyMax; i++)
 			{
-				enemyY = 0;
-				enemyX = rand() % 1100;
-				score += 1;
+				if (enemyFlag[i] == 1)
+				{
+					if (abs(bulletX - enemyX[i]) < 100 && abs(bulletY - enemyY[i]) < 100)
+					{
+						bulletflag = 0;
+						enemyFlag[i] = 0;
+						score += 1;
+					}
+				}
 			}
 			//タイマー
 			timer -= 1;
 
 			//弾
 			//弾のスピード
-			bulletY -= 15;
+			if (bulletflag == 1)
+			{
+				bulletY -= 15;
+			}
 			if (bulletflag == 0)
 			{
 				//スペースを押したら弾が出る
@@ -112,10 +147,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		Novice::DrawEllipse(posX, posY, radius, radius, 0.0f, WHITE, kFillModeSolid);
-		Novice::DrawBox(enemyX-50,enemyY-50, 100,100,0.0f, BLACK, kFillModeSolid);
+		for (int i = 0; i < enemyMax; i++)
+		{
+			if (enemyFlag[i] == 1)
+			{
+				Novice::DrawBox(enemyX[i] - 50, enemyY[i] - 50, 100, 100, 0.0f, BLACK, kFillModeSolid);
+			}
+		}
 		Novice::ScreenPrintf(100,100,"score=%d",score);
-		Novice::DrawEllipse(bulletX, bulletY, radius, radius, 0.0f, RED, kFillModeSolid);
+		if (bulletflag == 1)
+		{
+			Novice::DrawEllipse(bulletX, bulletY, radius, radius, 0.0f, RED, kFillModeSolid);
+
+		}
 		Novice::ScreenPrintf(0, 0, "x=%d", timer);
+		Novice::DrawBox(10,10, timer/10, 10, 0.0f, BLACK, kFillModeSolid);
 		//時間制限
 		if (timer<0)
 		{
